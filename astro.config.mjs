b341a7371dcb691907d3e255c006052823f1ef81
somenoe <otashi24@gmail.com>
2024-04-11 00:11:17 +0700

    ➕ Add dependency: Alpine.js.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs()]
+});
